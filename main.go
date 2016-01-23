2d3c13d1b762e7cb004e610876c9c19174a3d467
zainhoda <zain@hedgechart.com>
2016-01-23 12:32:04 -0500

    hello world
    hello world

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
